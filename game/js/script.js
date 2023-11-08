5b0ce061b6b2f1e5d546c505c039340cd9bb8596
Ehsan Cyber <124343651+EhsanCyber313@users.noreply.github.com>
2023-11-08 16:08:53 +0330

    Update script.js

--- game/js/script.js
+++ game/js/script.js
@@ -1,9 +1,21 @@
-let $ = document
-let buttonEdit = $.querySelector('.button-edit')
-let nameLord = $.querySelector('#nameLord')
+let $ = document;
+let buttonEdit = $.querySelector('.button-edit');
+let nameLord = $.querySelector('#nameLord');
+const nameCountry = $.getElementById('nameCountry')
+const nameCity = $.getElementById('nameCity')
+
 
 buttonEdit.addEventListener('click', function () {
-    let input = prompt('نام جدیدی که میخواهید را وارد کنید:')
-    nameLord.innerHTML = input
-
+    let inputName = prompt('نام جدید لرد خود را وارد کنید:');
+    let inputCountry = prompt('نام جدید کشور خود را وارد کنید:');
+    let inputCity = prompt('نام جدید شهر خود را وارد کنید:');
+    localStorage.setItem('newLord', inputName);
+    localStorage.setItem('newCountry', inputCountry);
+    localStorage.setItem('newCity', inputCity);
+    nameLord.innerHTML = localStorage.getItem("newLord");
+    nameCountry.innerHTML = localStorage.getItem("newCountry");
+    nameCity.innerHTML = localStorage.getItem("newCity");
 })
+nameLord.innerHTML = localStorage.getItem("newLord");
+nameCountry.innerHTML = localStorage.getItem("newCountry");
+nameCity.innerHTML = localStorage.getItem("newCity");
